1b4e832a2388d996af67d622281e1718f9ebc464
tim4ik <tim4ik@yahoo.com>
2021-09-20 15:17:36 +0300

    Changed Lab1 report to reflect the changes in the Task2 source

--- Lab1/Task2/Main.c
+++ Lab1/Task2/Main.c
@@ -4,62 +4,6 @@ int main(void)
 {
 	int iEightDigit, iProduct=1, iZeroCount=0,iDigitCount=0, iDigit=0;
 	scanf_s("%i", &iEightDigit);
-	/*
-	int iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;
-
-	iDigit = iEightDigit % 10;
-	if (iDigit == 0)
-		iZeroCount++;
-	else
-		iProduct *= iDigit;
-	iEightDigit /= 10;*/
 nextDigit:
 	iDigit = iEightDigit % 10;
 	if (iDigit == 0)
