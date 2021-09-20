67253db3913574978a31f28386cbe3823e64dfd3
tim4ik <tim4ik@yahoo.com>
2021-09-20 15:12:38 +0300

    Changed Lab1 Task2 source to use goto instead of multiple ifs

--- Lab1/Task2/Main.c
+++ Lab1/Task2/Main.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-	int iEightDigit, iProduct=1, iZeroCount=0;
+	int iEightDigit, iProduct=1, iZeroCount=0,iDigitCount=0, iDigit=0;
 	scanf_s("%i", &iEightDigit);
+	/*
 	int iDigit = iEightDigit % 10;
 	if (iDigit == 0)
 		iZeroCount++;
@@ -58,8 +59,17 @@ int main(void)
 		iZeroCount++;
 	else
 		iProduct *= iDigit;
+	iEightDigit /= 10;*/
+nextDigit:
+	iDigit = iEightDigit % 10;
+	if (iDigit == 0)
+		iZeroCount++;
+	else
+		iProduct *= iDigit;
 	iEightDigit /= 10;
-
+	iDigitCount++;
+	if (iDigitCount < 8)
+		goto nextDigit;
 	printf("Double product: %i \n", iProduct * 2);
 	printf("Zero count: %i", iZeroCount);
 }
