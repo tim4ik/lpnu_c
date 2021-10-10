9741c68e0d2f2be2805b6d0901bd1ae8da438e02
tim4ik <tim4ik@yahoo.com>
2021-10-10 14:48:34 +0300

    Changed the Task2 source

--- Lab2/Task2/Main.c
+++ Lab2/Task2/Main.c
@@ -3,33 +3,38 @@
 
 int main(void)
 {
-	float fA, fB, fStep, fEps;
+	float fA, fB, fStep, fEps, fQSteps;
+	int iPrecision;
 startInput:
 	printf("Enter a...");
 	scanf_s("%f", &fA);
 	printf("Enter b...");
 	scanf_s("%f", &fB);
-	printf("Enter step...");
-	scanf_s("%f", &fStep);
+	printf("Enter quantity of steps...");
+	scanf_s("%f", &fQSteps);
+	fStep = (fB - fA) / (fQSteps-1);
 	printf("Enter eps...");
 	scanf_s("%f", &fEps);
+	printf("Enter precision...");
+	scanf_s("%i", &iPrecision);
 	if (fabs(fA) >= 1 || fabs(fB) >= 1 || fA > fB || fStep <= 0 || fEps <= 0)
 	{
 		printf("Incorrect data!");
 		goto startInput;
 	}
-	for (float fX = fA; fX <= fB; fX += fStep)
+	for (float fX = fA; fX<fB || fabs(fX-fB)<=fEps; fX += fStep)
 	{
-		float fElement = 1, fSum = 0;
+		float fElement = 1, fSum = 0, fPow = fX;
 		int fCount = 2;
 		while (fabs(fElement) > fEps)
 		{
 			fSum += fElement;
-			fElement = fCount * pow(fX, fCount - 1);
+			fElement = fCount * fPow;
 			if (fCount % 2 == 0)
 				fElement *= -1;
 			fCount++;
+			fPow *= fX;
 		}
-		printf("x = %.3f, Tailor = %.3f, formula = %.3f\n", fX, fSum, pow(1 + fX, -2));
+		printf("x = %.*f, Tailor = %.*f, formula = %.*f\n", iPrecision, fX, iPrecision, fSum, iPrecision, pow(1 + fX, -2));
 	}
 }
